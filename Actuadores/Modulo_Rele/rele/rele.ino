03a8e9344dba5c88970fb9f82d4f451593743f03
alex-a2n <146248368+alex-a2n@users.noreply.github.com>
2025-03-01 01:16:49 +0100

    Add files via upload

---
+++ Actuadores/Modulo_Rele/rele/rele.ino
@@ -0,0 +1,21 @@
+/*
+
+Autor: A2N
+Web: https://arduinlab.wordpress.com
+DESCRIPCIÓN DEL PROGRAMA: "blink" del relé
+
+*/
+
+int rele = 3;
+
+void setup() {
+  pinMode(rele, OUTPUT);
+}
+
+void loop() {
+  // Encender el relé
+  digitalWrite(rele, HIGH);
+  delay(1000);
+  digitalWrite(rele, LOW);
+  delay(1000);
+}
