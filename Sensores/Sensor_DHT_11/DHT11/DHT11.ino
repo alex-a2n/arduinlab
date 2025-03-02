ed2260c1be53affc8b429177366743b552281cbc
alex-a2n <146248368+alex-a2n@users.noreply.github.com>
2025-03-02 23:13:56 +0100

    Add files via upload

---
+++ Sensores/Sensor_DHT_11/DHT11/DHT11.ino
@@ -0,0 +1,33 @@
+/*
+
+Autor: A2N
+Web: https://arduinlab.wordpress.com
+DESCRIPCIÓN DEL PROGRAMA: Lectura DHT11
+
+*/
+
+#include <DHT.h>
+
+// #define DHTTYPE DHT11
+// #define DHTPIN 9
+
+DHT dht(9, DHT11);
+float humedad;
+float temperatura;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(1000);
+  humedad = dht.readHumidity();
+  temperatura = dht.readTemperature();
+  if(isnan(humedad) || isnan(temperatura)){
+    Serial.println("Error");
+    return;
+  }
+  Serial.print("Humedad: "); Serial.print(humedad);Serial.print("%");
+  Serial.print("Temperatura: "); Serial.print(temperatura);Serial.println("ºC");
+}
